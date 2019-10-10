40c2bd38f9bd339023f419f8709674be90bd8836
danisfabric <danisfabric@gmail.com>
2019-10-10 14:51:47 +0800

    Initial Commit

---
+++ PlayerHubSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerHubSample
+//
+//  Created by 廖雷 on 2019/10/10.
+//  Copyright © 2019 Danis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
